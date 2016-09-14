0a2613c8dc0b0586c7225e13668ff11160e504df
Steinar Þór Árnason <steinar@intrum.is>
2016-09-14 15:41:27 +0000

    hot reloading works

--- tools/devServer.js
+++ tools/devServer.js
@@ -1,18 +1,20 @@
-/* eslint-disable no-console, no-process-exit */
+/* eslint-disable no-console */
 import express from 'express';
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 
 import config from '../webpack.dev.config';
-import { DevStart, DevStop } from './messages';
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 const compiler = webpack(config);
 
 const wdm = webpackDevMiddleware(compiler, {
   publicPath: config.output.publicPath,
+  profile: true,
+  progress: true,
+  noInfo: true,
   stats: {
     colors: true
   }
@@ -27,20 +29,11 @@ const server = app.listen(PORT, 'localhost', (error) => {
     console.error(error);
     return;
   }
-  DevStart();
+
   console.log(`Listening at http://localhost:${ PORT }`);
 });
 
-process.on('SIGTERM', () => {
-  DevStop();
-  wdm.close();
-  server.close(() => {
-    process.exit(0);
-  });
-});
-
 process.on('SIGINT', () => {
-  DevStop();
   wdm.close();
   server.close(() => {
     process.exit(0);
