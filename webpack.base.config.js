d029a9539e947bed5001989fc2eb8f64d758a1a7
Steinar Þór Árnason <steinar@intrum.is>
2016-09-15 18:11:16 +0000

    loader moved to their specific build types

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -1,12 +1,6 @@
 import path from 'path';
 
 export default {
-  module: {
-    loaders: [
-      { test: /\.jsx?$/, loaders: [ 'babel' ], exclude: /node_modules/ },
-    ],
-  },
-
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
