31e40670dfc5d8b150956604d88feeeb75603710
Steinar Þór Árnason <steinar@intrum.is>
2016-09-19 11:06:15 +0000

    added basic css-modules files

--- app/index.jsx
+++ app/index.jsx
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import { Router, hashHistory } from 'react-router';
 
 import routes from './routes';
+import styles from 'global.css'; // eslint-disable-line
 
 render(
   <Router history={ hashHistory } routes={ routes } />,
